a0e67d9991b98646db8f92691f3a101a918cd57d
Lucas Calebe <lucas.aguiar@ilia.digital>
2022-04-25 11:32:34 -0300

    🚧 add router to list component from home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
+import { ListComponent } from './list/list.component';
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 
 export const ROUTES: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'pokemons', component: ListComponent}
 ]
