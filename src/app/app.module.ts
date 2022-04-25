52acb7ff8ded9adaba157b41e7062a2f2e951fbf
Brian Miranda <brianmiranda.dev@gmail.com>
2022-04-25 02:18:45 -0400

    🔧 Routing config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { PreloadAllModules, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ROUTES } from './app.routes';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
   providers: [],
   bootstrap: [AppComponent]
